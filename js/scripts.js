34962e8b4b8f060865cccefe8cebfd7fefa23452
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-19 22:58:57 -0300

    Inicializa as principais variaveis

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
