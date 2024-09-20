2c02d4d385b9a402c0da6849cdab8e9b9300166c
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-09-19 23:47:51 -0300

    Adiciona atualização do resultado e manipulação de clique dos botões

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ let currentNumber = "";
 let firstOperand = null;
 let operator = null;
 let restart = false;
+
+function updateResult(originClear = false) {
+	result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const buttonText = button.innerText;
+		if (/^[0-9,]+$/.test(buttonText)) {
+			addDigit(buttonText);
+		}
+	});
+});
